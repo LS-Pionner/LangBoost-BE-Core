9143822217a6ec3fc5379f13d69a9c0f4875d4e6
jimmy <hojun.korea@gmail.com>
2024-12-18 19:17:56 +0900

    feat: swaggerConfig 추가

--- src/main/java/com/example/memoria/config/SwaggerConfig.java
+++ src/main/java/com/example/memoria/config/SwaggerConfig.java
@@ -1,27 +1,31 @@
 package com.example.memoria.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import lombok.RequiredArgsConstructor;
-import org.springdoc.core.models.GroupedOpenApi;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.headers.Header;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(title = "Memo API 명세서",
-                description = "Memo API 명세서",
-                version = "v1"))
-@RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public GroupedOpenApi v1OpenApi() {
-        // "/v1/**" 경로에 매칭되는 API를 그룹화하여 문서화한다.
-        String[] paths = {"/v1/**"};
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addHeaders("x-passport", new Header()
+                                .description("Serialized Passport Object")
+                                .schema(new StringSchema()))) // x-passport 헤더 추가
+                .info(apiInfo());
+    }
 
-        return GroupedOpenApi.builder()
-                .group("Memo API v1")  // 그룹 이름을 설정한다.
-                .pathsToMatch(paths)     // 그룹에 속하는 경로 패턴을 지정한다.
-                .build();
+    private Info apiInfo() {
+        return new Info()
+                .title("LB Core Server API Test") // API의 제목
+                .description("LangBoost Core Server API Test") // API에 대한 설명
+                .version("1.0.0"); // API의 버전
     }
 }
+
